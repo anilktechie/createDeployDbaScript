35bc127a1ee8df4e7bba226fe290f6eb52b221cc
Esteban Salmeron <esteban.salmeron@bestseller.com>
2021-12-01 12:40:25 +0100

    Create initial version

---
+++ createDeployDbaScript.py
@@ -0,0 +1,32 @@
+#----------------------------------------------------------------------------
+# Script for create deploy_dba.sql and deploy_dba.zip files
+# Run it within the folder that contains all the sql packages to be deployed
+# Created by Esteban Salmeron - 2021
+# V1.0    30-11-2021  Creation
+#----------------------------------------------------------------------------
+
+import os
+import zipfile
+
+# Type US info here!
+userStory = 'ISITF 1222 - Stop using BestInfoII System user' + '\n'
+
+zip = zipfile.ZipFile('deploy_dba.zip','a')
+f = open('deploy_dba.sql', 'w')
+
+f.write('--'+ userStory)
+f.write('prompt SALES TEAM deployment starting \n')
+f.write('set define off \n')
+f.write('\n')
+
+for root, dirs, files in os.walk('./'):
+    for newname in files:
+        filename = os.path.join(newname)
+        if filename != 'deploy_dba.sql' and filename.endswith('.sql'):
+            zip.write(filename)
+            newstring = 'prompt ' + filename + '\n' + '@' + filename + '\n' + '\n'
+            f.write(newstring)
+
+f.close()
+zip.write('deploy_dba.sql')
+zip.close()
